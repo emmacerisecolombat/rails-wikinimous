a7ca5fc2011e1cb3a72580569a36e170892ad17f
Emma Colombat <emma.colombat69@gmail.com>
2020-08-12 11:26:59 +0200

    Rails Wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/show'
-  get 'articles/new'
-  get 'articles/create'
-  get 'articles/edit'
-  get 'articles/update'
-  get 'articles/delete'
+resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :article
+
 end
